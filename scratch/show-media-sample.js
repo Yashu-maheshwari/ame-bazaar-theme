0cdf58ae408c0df183a4d09af491029a1eaf9315
AME Bazaar Dev <contact@amebazaar.com>
2026-07-07 17:24:42 +0530

    chore: remove scratch scripts

--- scratch/show-media-sample.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const media = JSON.parse(fs.readFileSync('C:/Users/user/.gemini/antigravity/brain/217da65a-e7e1-429e-afe7-5175d47a2cc6/scratch/media-library.json', 'utf8'));
-
-console.log('--- FIRST 50 ATTACHMENTS ---');
-media.slice(0, 50).forEach(item => {
-  console.log('ID: ' + item.id + ' | Title: ' + item.title + ' | Slug: ' + item.name + ' | URL: ' + item.url);
-});
