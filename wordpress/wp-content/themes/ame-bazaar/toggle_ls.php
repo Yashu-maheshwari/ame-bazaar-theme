f9f57bee7366c6a0d3caee8c05f9376cd34bac05
Yashu-maheshwari <cute.yashu09@gmail.com>
2026-09-12 00:15:19 +0530

    Temp: Add toggle LS script

---
+++ wordpress/wp-content/themes/ame-bazaar/toggle_ls.php
@@ -0,0 +1,22 @@
+<?php
+require_once('../../../wp-load.php');
+if (isset($_GET['secret_key']) && $_GET['secret_key'] === 'ame123') {
+    $action = $_GET['action'];
+    if ($action === 'off') {
+        update_option('litespeed.conf.optm-css_comb', '');
+        echo "CSS Combine set to OFF. ";
+    } elseif ($action === 'on') {
+        update_option('litespeed.conf.optm-css_comb', '1');
+        echo "CSS Combine set to ON. ";
+    }
+    
+    // Attempt to purge cache
+    if (class_exists('\LiteSpeed\Purge')) {
+        \LiteSpeed\Purge::purge_all();
+        echo "Purged via LiteSpeed\Purge. ";
+    } else {
+        echo "Class LiteSpeed\Purge not found. ";
+    }
+} else {
+    echo "Access denied";
+}
